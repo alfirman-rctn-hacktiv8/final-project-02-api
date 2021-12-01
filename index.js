95e46e777d7c432295476819035253bd1e750809
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2021-12-01 19:57:47 +0700

    fix: try

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000","https://codepen.io"] }));
 app.use(express.json());
 
 app.use("/user", user);
