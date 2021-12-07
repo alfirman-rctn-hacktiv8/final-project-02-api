6e8296996887eb659b21ce15b1e46ee65c3473a2
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2021-12-07 17:23:32 +0700

    fix: error

--- middleware/adminOnly.js
+++ middleware/adminOnly.js
@@ -4,18 +4,18 @@ const User = require("../models/user");
 const adminOnly = async (req, res, next) => {
   const token = req.cookies?.jwt;
 
-  if (!token) return res.status(401).json({ message: "unauthenticated" });
+  if (!token) return res.status(401).json({ error: "unauthenticated" });
 
   const claims = jwt.verify(token, process.env.SECRET_KEY);
 
-  if (!claims) return res.status(401).json({ message: "unauthenticated" });
+  if (!claims) return res.status(401).json({ error: "unauthenticated" });
 
   const admin = await User.findById(claims._id);
 
-  if (!admin) return res.status(401).json({ message: "unauthenticated" });
+  if (!admin) return res.status(401).json({ error: "unauthenticated" });
 
   if (admin._id != process.env.ADMIN_ID)
-    return res.status(403).json({ message: "forbidden" });
+    return res.status(403).json({ error: "forbidden" });
 
   next();
 };
